68b3a36b55fdafc18135dc5d0b6b31aeef92ab88
Marcin Wicha <marcin_wicha@outlook.com>
2018-12-15 22:53:03 +0100

    Started work on feat 16

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('.popup-youtube').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+});
