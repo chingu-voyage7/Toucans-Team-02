561b0ba88d0bb696e2268215dbd1d68d2833b769
Marcin Wicha <marcin_wicha@outlook.com>
2018-12-16 20:13:32 +0100

    Completed Magnific modal popup with Youtube - has to run through server. Completed thumbnail view - image will resize properly when inside container. Completed button mark up.
    Button styling to be continued.

--- js/main.js
+++ js/main.js
@@ -6,6 +6,27 @@ $(document).ready(function() {
 		removalDelay: 160,
 		preloader: false,
 
-		fixedContentPos: false
+		fixedContentPos: false,
+		iframe: {
+			markup: '<div class="mfp-iframe-scaler">'+
+					  '<div class="mfp-close"></div>'+
+					  '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+					'</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+		  
+			patterns: {
+			  youtube: {
+				index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+		  
+				id: 'v=', // String that splits URL in a two parts, second part should be %id%
+				// Or null - full URL will be returned
+				// Or a function that should return %id%, for example:
+				// id: function(url) { return 'parsed id'; }
+		  
+				src: 'http://www.youtube.com/embed/%id%' // URL that will be set as a source for iframe.
+			  }
+			},
+		  
+			srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+		  }
 	});
 });
