c4d4ca2c0f4885b6a4915c75bb0bd1664c852e02
Marcin Wicha <marcin_wicha@outlook.com>
2018-12-18 20:10:49 +0100

    Finished work on Feature #16, implemented counter for feature #14 and #15. Implemented counterup.js for Feature #14 and #15. Added timeout to pause counterup.js until Wow.js animations finish.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,82 @@
+/* start counters */
+	/* Init definition:
+	var counter = new CountUp("elementId", starValue, endValue, decimalPts, duration in seconds);
+	*/
+	
+	/* init counter-1 */
+	var counterOneAnim = new CountUp("counter-1", 0, 2.5, 1, 2);
+	/* init counter-1 */
+
+	/* init counter-2 */
+	var counterTwoAnim = new CountUp("counter-2", 0, 2.6, 1, 2);
+	/* init counter-2 */
+
+	/* init counter-3 */
+	var counterThreeAnim = new CountUp("counter-3", 0, 250, 0, 2);
+	/* end counter-3 */
+
+	/* init counter-4 */
+	var counterFourAnim = new CountUp("counter-4", 0, 2.6, 1, 2);
+	/* end counter-1 4/
+/* end counters */
+
+/* start WOW.js */
+var wow = new WOW(
+	{
+		boxClass: 'wow',
+		animateClass: 'animated',
+		offset: 0,
+		mobile: true,
+		live: true,
+		/* start counter-timeout */
+		/* Pause counterup.js until wow.js finishes animating the element */ 
+		callback: function(box){ // call function when animation starts
+			if(box.querySelector("span.counter") != null){ // check if animated element has a counter.js child
+				/* start counter-1 timeout */
+				if(box.querySelector("span.counter").id == "counter-1"){ // check if child counter id is counter-1
+					if (!counterOneAnim.error) {
+						setTimeout(function(){counterOneAnim.start();}, 1000); // pause counterup for 1000ms (animation duration + 100ms)
+					} else {
+							console.error(counterOneAnim.error);
+					}
+				}
+				/* end counter-1 timeout */
+				/* start counter-2 timeout */
+				if(box.querySelector("span.counter").id == "counter-2"){
+					if (!counterTwoAnim.error) {
+						setTimeout(function(){counterTwoAnim.start();}, 1200);
+					} else {
+							console.error(counterTwoAnim.error);
+					}
+				}
+				/* end counter-2 timeout */
+				/* start counter-3 timeout */
+				if(box.querySelector("span.counter").id == "counter-3"){
+					if (!counterThreeAnim.error) {
+						setTimeout(function(){counterThreeAnim.start();}, 1000);
+					} else {
+							console.error(counterThreeAnim.error);
+					}
+				}
+				/* end counter-3 timeout */
+				/* start counter-4 timeout */
+				if(box.querySelector("span.counter").id == "counter-4"){
+					if (!counterFourAnim.error) {
+						setTimeout(function(){counterFourAnim.start();}, 1200);
+					} else {
+							console.error(counterFourAnim.error);
+					}
+				}
+				/* end counter-4 timeout */
+			}
+		}, 
+		/*end counter-timeout */
+		scrollContainer: null
+	}
+);
+wow.init();
+/* end WoW.js */
+
 /* start popup-youtube */
 $(document).ready(function() {
 	$('.popup-youtube').magnificPopup({
